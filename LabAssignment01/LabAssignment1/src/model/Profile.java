89008da58ec63924c60cf6b6c141d02f28dce2a8
Mohammed Omer Qasim Shaik <mohammedomerqasim@gmail.com>
2022-12-20 14:35:55 -0500

    added LabAssignment01

---
+++ LabAssignment01/LabAssignment1/src/model/Profile.java
@@ -0,0 +1,194 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author omerq
+ */
+public class Profile {
+    
+    private String firstName;
+    private String lastName;
+    private String dateOfBirth;
+    private String age;
+    private String telNum;
+    private String email;
+    private String pAddress;
+    private String sAddress;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String primaryDegree;
+    private String pDegreeStart;
+    private String pDegreeEnd;
+    private String secondaryDegree;
+    private String sDegreeStart;
+    private String sDegreeEnd;
+    private String userPhoto;
+
+    
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(String telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getpAddress() {
+        return pAddress;
+    }
+
+    public void setpAddress(String pAddress) {
+        this.pAddress = pAddress;
+    }
+
+    public String getsAddress() {
+        return sAddress;
+    }
+
+    public void setsAddress(String sAddress) {
+        this.sAddress = sAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getPrimaryDegree() {
+        return primaryDegree;
+    }
+
+    public void setPrimaryDegree(String primaryDegree) {
+        this.primaryDegree = primaryDegree;
+    }
+
+    public String getpDegreeStart() {
+        return pDegreeStart;
+    }
+
+    public void setpDegreeStart(String pDegreeStart) {
+        this.pDegreeStart = pDegreeStart;
+    }
+
+    public String getpDegreeEnd() {
+        return pDegreeEnd;
+    }
+
+    public void setpDegreeEnd(String pDegreeEnd) {
+        this.pDegreeEnd = pDegreeEnd;
+    }
+
+    public String getSecondaryDegree() {
+        return secondaryDegree;
+    }
+
+    public void setSecondaryDegree(String secondaryDegree) {
+        this.secondaryDegree = secondaryDegree;
+    }
+
+    public String getsDegreeStart() {
+        return sDegreeStart;
+    }
+
+    public void setsDegreeStart(String sDegreeStart) {
+        this.sDegreeStart = sDegreeStart;
+    }
+
+    public String getsDegreeEnd() {
+        return sDegreeEnd;
+    }
+
+    public void setsDegreeEnd(String sDegreeEnd) {
+        this.sDegreeEnd = sDegreeEnd;
+    }
+
+    public String getUserPhoto() {
+        return userPhoto;
+    }
+
+    public void setUserPhoto(String userPhoto) {
+        this.userPhoto = userPhoto;
+    }
+      
+}
---
+++ LabAssignment01/README.md
@@ -0,0 +1,37 @@
+# Lab Assignment - 1
+
+Your task is to build a java application that will allow a user to create a user profile and then display the results.
+
+The two use cases are:
+
+1. Create a user profile
+2. Display this user profile
+
+In this exercise, you are to define a user’s class with the following attributes.
+
+First Name, 
+Last Name, 
+DOB, 
+Age, 
+Telephone number, 
+Email, 
+Street Line 1, 
+Street Line 2, 
+City, 
+Country, 
+Affiliation, 
+Major, 
+Career, 
+Degree 1, 
+Degree 1 Start, 
+Degree 1 End, 
+Degree 2, 
+Degree 2 Start, 
+Degree 2 End, 
+Photo
+ 
+
+Requirement:
+
+1. The photo image of the user is required.
+2. Upload your assignment onto GitHub. Submission on canvas is not acceptable.
